fbec67110bc045767201a906d5f8f3f7e16157bc
Sergio <Sergio99_git@outlook.com>
2024-01-05 16:47:15 -0600

    Ultimos avances al proyecto de cafeteria

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 //Requerimos extraer las dependencias
 const { src, dest, watch, series } = require('gulp');
+
+//CSS Y SASS
 const sass =  require('gulp-sass')(require('sass'));
 const postcss =  require('gulp-postcss');
 const autoprefixer =  require('autoprefixer');
+// var globalThis = require('globalthis/polyfill')();
+
+//IMAGENES
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css(done){
     //Compilar sass 
@@ -19,14 +27,43 @@ function dev(){
     watch('src/scss/**/*.scss',css);//Buscaremos todos los archivos que tengan la extension de scss
 }
 
-function tareaDefault(done){
-    console.log('Soy la tarea por default');
+// function tareaDefault(done){
+//     console.log('Soy la tarea por default');
+//     done();
+// }
+
+function primerTarea(done){
+    console.log('Primer tarea');
     done();
+}
+
+function images(done){
+    src('src/img/**/*')
+        .pipe( imagemin( { optimizationLevel: 3 } ) )
+        .pipe(dest('build/img'));
+    done();
+}
+
+function versionWebp(){
+    //Solo requerimos estas extensiones
+    return src('src/img/**/*.{png,jpg}')
+        .pipe( webp() ) //Convertirla en webp
+        .pipe( dest('build/img') )
+}
+
+function versionAvif(){
+    return src('src/img/**/*.{png,jpg}')
+        .pipe( avif() )
+        .pipe ( dest('build/img') )
 }
   
 exports.css = css;
 exports.dev = dev;
-exports.default = series(css,dev);
+exports.images = images;
+exports.versionWebp = versionWebp; 
+exports.versionAvif = versionAvif;
+exports.default = series(images,versionWebp,versionAvif,css,dev);
+exports.primerTarea = primerTarea;
 
 //series- se inicia una tarea y hasta que finaliza inicia la siguiente
 
